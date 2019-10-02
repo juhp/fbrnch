47d1736a37175929ebe5bdfed23e8de5e5007b02
Jens Petersen <petersen@redhat.com>
2019-10-02 15:14:24 +0800

    initial basic poc

--- Main.hs
+++ Main.hs
@@ -1,4 +1,23 @@
 module Main (main) where
 
+import SimpleCmd
+import SimpleCmd.Git
+import SimpleCmdArgs
+
 main :: IO ()
-main = putStrLn "hello"
+main =
+  simpleCmdArgs Nothing "Fedora package branch building tool"
+    "This tool helps with updating and building package branches" $
+    run <$> strArg "FROMBRANCH" <*> strArg "TOBRANCH"
+
+run :: String -> String -> IO ()
+run frombrnch tobrnch = do
+  fedpkg "switch-branch" [tobrnch]
+  git_ "diff" [frombrnch]
+  git_ "merge" [frombrnch]
+  fedpkg "push" []
+  fedpkg "build" []
+
+fedpkg :: String -> [String] -> IO ()
+fedpkg c args =
+  cmd_ "fedpkg" (c:args)
