31c69a3edfdee0e9075735753c1c906fc7c9ffd4
Jens Petersen <petersen@redhat.com>
2020-04-11 03:22:57 +0800

    move <> from Common.Text to Common
    to avoid T.<>

--- src/Common.hs
+++ src/Common.hs
@@ -1,9 +1,20 @@
+{-# LANGUAGE CPP #-}
+
 module Common (
   module Control.Monad,
   module Data.List,
   module Data.Maybe
-              ) where
+#if (defined(MIN_VERSION_base) && MIN_VERSION_base(4,11,0))
+#else
+, (<>)
+#endif
+  ) where
 
 import Control.Monad
 import Data.List
 import Data.Maybe
+
+#if (defined(MIN_VERSION_base) && MIN_VERSION_base(4,11,0))
+#else
+import Data.Semigroup ((<>))
+#endif
