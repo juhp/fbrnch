3a2c1818b3f2a4d86dcd2cb94d044c19d9625680
Jens Petersen <petersen@redhat.com>
2020-07-21 13:04:52 +0800

    InterleaveOutput: import <> for older ghc <= 8.2

--- src/InterleaveOutput.hs
+++ src/InterleaveOutput.hs
@@ -1,9 +1,15 @@
+{-# LANGUAGE CPP #-}
+
 module InterleaveOutput (cmdSilent_) where
 
 import qualified Data.ByteString.Lazy.Char8 as B
 import System.Exit (exitWith, ExitCode (..))
 import System.Process (showCommandForUser)
 import System.Process.Typed (proc, readProcessInterleaved)
+
+#if !MIN_VERSION_base(4,11,0)
+import Data.Semigroup ((<>))
+#endif
 
 cmdSilent_ :: String -> [String] -> IO ()
 cmdSilent_ c args = do
