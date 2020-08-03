406c7bb008e809984e72dd7c9ee6228cb17ec9c0
Jens Petersen <petersen@redhat.com>
2020-08-04 01:37:02 +0800

    InterleaveOutput: simplify to readProcessInterleaved_

--- src/InterleaveOutput.hs
+++ src/InterleaveOutput.hs
@@ -2,20 +2,14 @@
 
 module InterleaveOutput (cmdSilent_) where
 
-import qualified Data.ByteString.Lazy.Char8 as B
-import System.Exit (exitWith, ExitCode (..))
-import System.Process (showCommandForUser)
-import System.Process.Typed (proc, readProcessInterleaved)
+import System.Process.Typed (proc, readProcessInterleaved_)
+
+import Common
 
 #if !MIN_VERSION_base(4,11,0)
 import Data.Semigroup ((<>))
 #endif
 
 cmdSilent_ :: String -> [String] -> IO ()
-cmdSilent_ c args = do
-  (ret, out) <- readProcessInterleaved $ proc c args
-  case ret of
-    ExitSuccess -> return ()
-    ExitFailure n -> do
-      B.putStrLn $ B.pack (showCommandForUser c args ++ " failed with status " ++ show n ++ "\n") <> out
-      exitWith $ ExitFailure n
+cmdSilent_ c args =
+  void $ readProcessInterleaved_ $ proc c args
