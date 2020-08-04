70e945c5184e8ecd62e6d8d870fef5be6ff6298e
Jens Petersen <petersen@redhat.com>
2020-08-05 00:40:24 +0800

    stack rpmbuild-order-0.4.2

--- src/InterleaveOutput.hs
+++ src/InterleaveOutput.hs
@@ -6,10 +6,6 @@ import System.Process.Typed (proc, readProcessInterleaved_)
 
 import Common
 
-#if !MIN_VERSION_base(4,11,0)
-import Data.Semigroup ((<>))
-#endif
-
 cmdSilent_ :: String -> [String] -> IO ()
 cmdSilent_ c args =
   void $ readProcessInterleaved_ $ proc c args
