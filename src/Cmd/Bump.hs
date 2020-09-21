057546fdaf3a23d6a3aa0e886895ab9230265882
Jens Petersen <petersen@redhat.com>
2020-09-21 15:14:06 +0800

    bump: check koji for latest build to see if bump needed
    theoretically should take target to check but this seems good enough

--- src/Cmd/Bump.hs
+++ src/Cmd/Bump.hs
@@ -4,32 +4,45 @@ module Cmd.Bump
 where
 
 import Branches
+import Common
 import Common.System
 import Git
+import Koji
 import Package
 
--- FIXME check koji if bump needed
 bumpPkgs :: Maybe CommitOpt -> Maybe BranchOpts -> [String] -> IO ()
 bumpPkgs mopt mbrnchopts =
   withPackageByBranches (Just True) cleanGitFetchActive mbrnchopts Nothing bumpPkg
   where
     bumpPkg :: Package -> AnyBranch -> IO ()
     bumpPkg pkg br = do
-      -- latest <- kojiLatestPkg dist pkg
-      -- when (eqNVR nvr latest) $ do
+      -- FIXME or check for unpushed bump
+      mlatest <-
+        case br of
+          RelBranch rbr ->
+            let tag = branchDestTag rbr in
+              kojiLatestNVR tag $ unPackage pkg
+          _ -> return Nothing
       spec <- localBranchSpecFile pkg br
-      git_ "log" ["origin..HEAD", "--pretty=oneline"]
-      let clmsg =
-            case mopt of
-              Just (CommitMsg msg) -> msg
-              _ -> "rebuild"
-      cmd_ "rpmdev-bumpspec" ["-c", clmsg, spec]
-      let copts =
-            case mopt of
-              Nothing -> ["-m", "bump release"]
-              Just opt ->
-                case opt of
-                  CommitMsg msg -> ["-m", msg]
-                  -- FIXME reject amend if already pushed
-                  CommitAmend -> ["--amend", "--no-edit"]
-      git_ "commit" $ "-a" : copts
+      rbr <-
+        case br of
+          RelBranch rbr -> return rbr
+          OtherBranch _ -> systemBranch
+      nvr <- pkgNameVerRel' rbr spec
+      if equivNVR nvr (fromMaybe "" mlatest) then do
+        git_ "log" ["origin..HEAD", "--pretty=oneline"]
+        let clmsg =
+              case mopt of
+                Just (CommitMsg msg) -> msg
+                _ -> "rebuild"
+        cmd_ "rpmdev-bumpspec" ["-c", clmsg, spec]
+        let copts =
+              case mopt of
+                Nothing -> ["-m", "bump release"]
+                Just opt ->
+                  case opt of
+                    CommitMsg msg -> ["-m", msg]
+                    -- FIXME reject amend if already pushed
+                    CommitAmend -> ["--amend", "--no-edit"]
+        git_ "commit" $ "-a" : copts
+        else putStrLn "already bumped"
