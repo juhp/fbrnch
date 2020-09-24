e117c24c7bdd5f925e941bd83f5be12ffaf4e555
Jens Petersen <petersen@redhat.com>
2020-09-24 23:21:16 +0800

    Mock: format with ormolu

--- src/Cmd/Mock.hs
+++ src/Cmd/Mock.hs
@@ -1,5 +1,6 @@
-module Cmd.Mock (
-  mockCmd)
+module Cmd.Mock
+  ( mockCmd,
+  )
 where
 
 import Branches
@@ -10,21 +11,23 @@ import Package
 
 mockCmd :: Maybe Branch -> [String] -> IO ()
 mockCmd mroot args = do
-  (brs,pkgs) <- splitBranchesPkgs True Nothing args
+  (brs, pkgs) <- splitBranchesPkgs True Nothing args
   if null pkgs
     then do
-    unlessM isPkgGitRepo $
-      error' "Please specify at least one package"
+      unlessM isPkgGitRepo $
+        error' "Please specify at least one package"
     else do
-    whenM isPkgGitRepo $
-      error' "Cannot build multiple packages inside a package dir"
+      whenM isPkgGitRepo $
+        error' "Cannot build multiple packages inside a package dir"
   when (null brs && length pkgs > 1 && isNothing mroot) $
     error' "Must specific branch or --root chroot"
   branches <-
     if null brs
-    then if null pkgs then pure <$> getReleaseBranch
-         else pure <$> systemBranch
-    else map onlyRelBranch <$> listOfBranches False False Nothing brs
+      then
+        if null pkgs
+          then pure <$> getReleaseBranch
+          else pure <$> systemBranch
+      else map onlyRelBranch <$> listOfBranches False False Nothing brs
   let packages = if null pkgs then ["."] else pkgs
   mapM_ (mockBuildPkgs (null brs) packages) branches
   where
@@ -39,7 +42,7 @@ mockCmd mroot args = do
               [pkg] ->
                 let mverrel = stripInfix "-" $ removePrefix (pkg ++ "-") $ takeNVRName (head srpms)
                     verrel = maybe "" (uncurry (</>)) mverrel
-                in ["--resultdir=results_" ++ pkg </> verrel]
+                 in ["--resultdir=results_" ++ pkg </> verrel]
               _ -> []
       let command = if length pkgs > 1 then "--chain" else "--rebuild"
       cmd_ "mock" $ [command, "--root", mockConfig rootBr] ++ resultdir ++ srpms
@@ -47,12 +50,15 @@ mockCmd mroot args = do
         prepSrpm :: AnyBranch -> FilePath -> IO FilePath
         prepSrpm rbr pkgdir =
           withExistingDirectory pkgdir $ do
-          pkg <- getPackageName pkgdir
-          putPkgHdr pkg
-          actualBr <-
-            ifM (notM isPkgGitRepo)
-            (return rbr)
-            (if noswitch then gitCurrentBranch
-             else gitSwitchBranch rbr >> return rbr)
-          spec <- findSpecfile
-          (pkgdir </>) . takeFileName <$> generateSrpm (Just actualBr) spec
+            pkg <- getPackageName pkgdir
+            putPkgHdr pkg
+            actualBr <-
+              ifM
+                (notM isPkgGitRepo)
+                (return rbr)
+                ( if noswitch
+                    then gitCurrentBranch
+                    else gitSwitchBranch rbr >> return rbr
+                )
+            spec <- findSpecfile
+            (pkgdir </>) . takeFileName <$> generateSrpm (Just actualBr) spec
