e2d3af1a3abf6628edc369ba5edf2d5639c1985e
Jens Petersen <juhpetersen@gmail.com>
2022-03-03 20:33:15 +0800

    status: just warn don't error if branch does not exist
    eg for ghc-attempt dead.package

--- src/Cmd/Status.hs
+++ src/Cmd/Status.hs
@@ -35,68 +35,74 @@ statusBranch :: Package -> AnyBranch -> IO ()
 statusBranch _ (OtherBranch _) =
   error' "status currently only defined for release branches"
 statusBranch pkg rbr@(RelBranch br) = do
-  gitSwitchBranch rbr
-  let spec = packageSpec pkg
-  ifM (notM (doesFileExist spec))
-    (ifM initialPkgRepo
-      (putStrLn $ show br ++ ": initial repo")
-      (putStrLn $ "missing " ++ spec)) $
-    do
-    mnvr <- pkgNameVerRel br spec
-    case mnvr of
-      Nothing -> do
-        putStrLn "undefined NVR!\n"
-        putStr "HEAD "
-        gitShortLog1 Nothing >>= putStrLn
-      Just nvr -> do
-        -- unless (br == Rawhide) $ do
-        --   newerBr <- newerBranch br <$> getFedoraBranches
-        --   ancestor <- gitBool "merge-base" ["--is-ancestor", "HEAD", show newerBr]
-        --   when ancestor $ do
-        --     unmerged <- gitShortLog $ "HEAD..origin/" ++ show newerBr
-        --     unless (null unmerged) $ do
-        --       putStrLn $ "Newer commits in " ++ show newerBr ++ ":"
-        --       mapM_ putStrLn unmerged
-        unpushed <- gitShortLog1 $ Just $ "origin/" ++ show br ++ "..HEAD"
-        if null unpushed then do
-          mbuild <- kojiGetBuildID fedoraHub nvr
-          case mbuild of
-            Nothing -> do
-              mlatest <- kojiLatestNVR (branchDestTag br) (unPackage pkg)
-              case mlatest of
-                Nothing -> putStrLn $ "new " ++ nvr
-                Just latest ->
-                  putStrLn $ if equivNVR nvr latest then latest ++ " is latest modulo disttag" else (if null latest then "new " else (head . words) latest ++ " ->\n") ++ nvr
-            Just buildid -> do
-              tags <- kojiBuildTags fedoraHub (buildIDInfo buildid)
-              if null tags then do
-                mstatus <- kojiBuildStatus nvr
-                -- FIXME show pending archs building
-                whenJust mstatus $ \ status ->
-                  -- FIXME better Show BuildStatus
-                  putStr $ nvr ++ " (" ++ show status ++ ")"
-              else do
-                -- FIXME hide testing if ga/stable
-                putStr $ nvr ++ " (" ++ unwords tags ++ ")"
-                unless (isStable tags) $ do
-                  updates <- bodhiUpdates
-                             [makeItem "display_user" "0",
-                              makeItem "builds" nvr]
-                  case updates of
-                    [] -> putStrLn "No update found"
-                    [update] -> do
-                      -- FIXME could show minus time left using stable_days?
-                      let msince = lookupKey "date_testing" update :: Maybe LocalTime
-                      case msince of
-                        Nothing -> return ()
-                        Just date -> do
-                          let since = localTimeToUTC utc date
-                          current <- getCurrentTime
-                          let diff = diffUTCTime current since
-                          putAge diff
-                    _ -> putStrLn "More than one update found!"
-              putStrLn ""
-          else putStrLn $ show br ++ ": " ++ unpushed
+  brExists <- checkIfRemoteBranchExists rbr
+  if not brExists
+    then do
+    name <- getDirectoryName
+    putStrLn $ name ++ " has no branch " ++ show br
+    else do
+    gitSwitchBranch rbr
+    let spec = packageSpec pkg
+    ifM (notM (doesFileExist spec))
+      (ifM initialPkgRepo
+        (putStrLn $ show br ++ ": initial repo")
+        (putStrLn $ "missing " ++ spec)) $
+      do
+      mnvr <- pkgNameVerRel br spec
+      case mnvr of
+        Nothing -> do
+          putStrLn "undefined NVR!\n"
+          putStr "HEAD "
+          gitShortLog1 Nothing >>= putStrLn
+        Just nvr -> do
+          -- unless (br == Rawhide) $ do
+          --   newerBr <- newerBranch br <$> getFedoraBranches
+          --   ancestor <- gitBool "merge-base" ["--is-ancestor", "HEAD", show newerBr]
+          --   when ancestor $ do
+          --     unmerged <- gitShortLog $ "HEAD..origin/" ++ show newerBr
+          --     unless (null unmerged) $ do
+          --       putStrLn $ "Newer commits in " ++ show newerBr ++ ":"
+          --       mapM_ putStrLn unmerged
+          unpushed <- gitShortLog1 $ Just $ "origin/" ++ show br ++ "..HEAD"
+          if null unpushed then do
+            mbuild <- kojiGetBuildID fedoraHub nvr
+            case mbuild of
+              Nothing -> do
+                mlatest <- kojiLatestNVR (branchDestTag br) (unPackage pkg)
+                case mlatest of
+                  Nothing -> putStrLn $ "new " ++ nvr
+                  Just latest ->
+                    putStrLn $ if equivNVR nvr latest then latest ++ " is latest modulo disttag" else (if null latest then "new " else (head . words) latest ++ " ->\n") ++ nvr
+              Just buildid -> do
+                tags <- kojiBuildTags fedoraHub (buildIDInfo buildid)
+                if null tags then do
+                  mstatus <- kojiBuildStatus nvr
+                  -- FIXME show pending archs building
+                  whenJust mstatus $ \ status ->
+                    -- FIXME better Show BuildStatus
+                    putStr $ nvr ++ " (" ++ show status ++ ")"
+                else do
+                  -- FIXME hide testing if ga/stable
+                  putStr $ nvr ++ " (" ++ unwords tags ++ ")"
+                  unless (isStable tags) $ do
+                    updates <- bodhiUpdates
+                               [makeItem "display_user" "0",
+                                makeItem "builds" nvr]
+                    case updates of
+                      [] -> putStrLn "No update found"
+                      [update] -> do
+                        -- FIXME could show minus time left using stable_days?
+                        let msince = lookupKey "date_testing" update :: Maybe LocalTime
+                        case msince of
+                          Nothing -> return ()
+                          Just date -> do
+                            let since = localTimeToUTC utc date
+                            current <- getCurrentTime
+                            let diff = diffUTCTime current since
+                            putAge diff
+                      _ -> putStrLn "More than one update found!"
+                putStrLn ""
+            else putStrLn $ show br ++ ": " ++ unpushed
   where
     isStable :: [String] -> Bool
     isStable = not . all ("-testing" `isSuffixOf`)
