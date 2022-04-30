4fd2568f9258125a685e6597444efd908bde074a
Jens Petersen <juhpetersen@gmail.com>
2022-04-30 20:56:21 +0800

    local: buildRPMS already times so don't need timeIO

--- src/Cmd/Local.hs
+++ src/Cmd/Local.hs
@@ -19,7 +19,6 @@ import Common
 import Common.System
 import Git
 import Package
-import Time
 
 localCmd :: Bool -> Maybe ForceShort -> [BCond] -> (BranchesReq, [String])
          -> IO ()
@@ -32,8 +31,7 @@ localCmd quiet mforceshort bconds =
       rpms <- if isJust mforceshort
               then return []
               else builtRpms br spec
-      timeIO $
-        void $ buildRPMs quiet mforceshort bconds rpms br spec
+      void $ buildRPMs quiet mforceshort bconds rpms br spec
 
 installDepsCmd :: (Maybe Branch,[String]) -> IO ()
 installDepsCmd =
