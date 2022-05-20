e027cab19f722dd98bc9eedd4be2909356480564
Jens Petersen <juhpetersen@gmail.com>
2022-05-21 01:13:16 +0800

    InterleaveOutput: fix cmdSilent' to error on failure
    prep was succeeding even when rpmbuild fails

--- src/InterleaveOutput.hs
+++ src/InterleaveOutput.hs
@@ -4,10 +4,12 @@ import Data.ByteString.Lazy.UTF8 as B
 import GHC.IO.Exception (ExitCode(ExitSuccess))
 import System.Process.Typed (proc, readProcessInterleaved)
 import Common
+import Common.System
 
 cmdSilent' :: String -> [String] -> IO ()
-cmdSilent' c args =
-  void $ cmdSilentBool c args
+cmdSilent' c args = do
+  ok <- cmdSilentBool c args
+  unless ok $ error' $ unwords (c:args) ++ ": failed"
 
 cmdSilentBool :: String -> [String] -> IO Bool
 cmdSilentBool c args = do
