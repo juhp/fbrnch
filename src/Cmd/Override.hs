13903e9df6ae19426b1e29b620d564fb9dfa94b7
Jens Petersen <juhpetersen@gmail.com>
2022-11-13 20:47:31 +0800

    override: rework --list and --expire to work on all package's overrides
    currently they ignore branches

--- src/Cmd/Override.hs
+++ src/Cmd/Override.hs
@@ -6,7 +6,8 @@ module Cmd.Override (
   )
 where
 
-import Fedora.Bodhi (bodhiOverride)
+import Data.Aeson (Object)
+import Fedora.Bodhi (bodhiOverrides)
 import Network.HTTP.Query
 
 import Common
@@ -23,17 +24,15 @@ import Prompt (yesno)
 data OverrideMode = OverrideCreate | OverrideList | OverrideExpire
   deriving Eq
 
--- FIXME debug option
--- FIXME option to expire all overrides
--- FIXME should list all overrides at once
+-- FIXME debug option?
 overrideCmd :: Bool -> OverrideMode -> Maybe Int -> Bool
             -> (BranchesReq, [String]) -> IO ()
-overrideCmd dryrun mode mduration nowait breqpkgs = do
-    unless (nowait || mode /= OverrideCreate) $
-      putStrLn "Overriding"
-    withPackagesByBranches (if mode == OverrideList then HeaderNone else HeaderMay) False cleanGitFetchActive AnyNumber overrideBranch breqpkgs
-    unless (nowait || mode /= OverrideCreate) $
-      waitrepoCmd dryrun WaitNoFetch Nothing breqpkgs
+overrideCmd dryrun OverrideCreate mduration nowait breqpkgs = do
+  unless nowait $
+    putStrLn "Overriding"
+  withPackagesByBranches HeaderMay False cleanGitFetchActive AnyNumber overrideBranch breqpkgs
+  unless nowait $
+    waitrepoCmd dryrun WaitNoFetch Nothing breqpkgs
   where
     overrideBranch :: Package -> AnyBranch -> IO ()
     overrideBranch _ (OtherBranch _) =
@@ -43,27 +42,43 @@ overrideCmd dryrun mode mduration nowait breqpkgs = do
       let spec = packageSpec pkg
       checkForSpecFile spec
       nvr <- pkgNameVerRel' br spec
-      case mode of
-        OverrideCreate -> do
-          putStrLn nvr
-          tags <- kojiNVRTags nvr
-          unless (any (`elem` tags) [show br, show br ++ "-updates", show br ++ "-override"]) $
-            unlessM (checkAutoBodhiUpdate br) $
-            bodhiCreateOverride dryrun mduration nvr
-        OverrideList -> do
-          moverride <- bodhiOverride nvr
-          whenJust moverride $ \override ->
-            case (lookupKey "expired_date" override :: Maybe String) of
-              Just date -> putStrLn date
-              Nothing ->
-                putStrLn $ nvr +-+
-                fromMaybe "" (lookupKey "expiration_date" override :: Maybe String)
-        OverrideExpire -> do
-          moverride <- bodhiOverride nvr
-          whenJust moverride $ \override ->
-            case (lookupKey "expired_date" override :: Maybe String) of
-              Just date -> putStrLn date
-              Nothing -> do
-                ok <- yesno $ "Expire override" +-+ nvr
-                when ok $
-                  cmd_ "bodhi" ["overrides", "edit", "--expire", nvr]
+      putStrLn nvr
+      tags <- kojiNVRTags nvr
+      unless (any (`elem` tags) [show br, show br ++ "-updates", show br ++ "-override"]) $
+        unlessM (checkAutoBodhiUpdate br) $
+        bodhiCreateOverride dryrun mduration nvr
+overrideCmd _dryrun OverrideList _mduration _nowait (_breq,pkgs) =
+  withPackages pkgs $
+  packageOverrides >=> mapM_ showOverride
+  where
+    showOverride override =
+      case (lookupKey "expired_date" override :: Maybe String) of
+        Just _expiry -> return ()
+        Nothing ->
+          whenJust (lookupKey "nvr" override) $ \nvr ->
+          putStrLn $ nvr +-+
+          fromMaybe "" (lookupKey "expiration_date" override :: Maybe String)
+overrideCmd _dryrun OverrideExpire _mduration _nowait (_breq,pkgs) =
+  withPackages pkgs $
+  packageOverrides >=> mapM_ expireOverride
+  where
+    expireOverride override =
+      case (lookupKey "expired_date" override :: Maybe String) of
+        Just _expired -> return ()
+        Nothing -> do
+          whenJust (lookupKey "nvr" override) $ \nvr -> do
+            ok <- yesno $ "Expire override" +-+ nvr
+            when ok $
+              cmd_ "bodhi" ["overrides", "edit", "--expire", nvr]
+
+withPackages :: [FilePath] -> (Package -> IO ()) -> IO ()
+withPackages pkgs act =
+  forM_ pkgs $ \pkgdir ->
+  withExistingDirectory pkgdir $
+  getPackageName "." >>= act
+
+packageOverrides :: Package -> IO [Object]
+packageOverrides pkg =
+  -- FIXME could filter by "releases" for Branch's
+  bodhiOverrides [makeItem "packages" (unPackage pkg),
+                  makeItem "expired" "0"]
