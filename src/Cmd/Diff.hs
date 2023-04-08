417b21892d77a47719b6d8ef7a9f1cb55855ca89
Jens Petersen <juhpetersen@gmail.com>
2023-04-08 21:37:10 +0800

    diff: handle missing and non-release branches

--- src/Cmd/Diff.hs
+++ src/Cmd/Diff.hs
@@ -35,56 +35,68 @@ diffCmd debug speconly work fmt ignorebumps mpatt mwbr =
   where
     diffPkg :: Package -> AnyBranch -> IO ()
     diffPkg pkg br = do
-      gitSwitchBranch br
-      speconlyNone <-
-        if speconly
-        then notM $ doesFileExist $ packageSpec pkg
-        else return False
-      if speconlyNone
-        then do
-        dead <- doesFileExist "dead.package"
-        unless dead $ putStrLn $ "no " ++ packageSpec pkg
-        else do
-        let contxt = case fmt of
-                       DiffContext n -> ["--unified=" ++ show n]
-                       DiffMinimal -> ["--unified=0"]
-                       DiffStatus -> ["--name-status"]
-                       -- FIXME hide "files changed" and "insertions" summary
-                       DiffStats -> ["--compact-summary"]
-                       _ -> []
-            (workOpts,workArgs) =
-              case work of
-                DiffWorkAll -> ([],["HEAD" | isNothing mwbr])
-                DiffWorkUnstage -> ([],[])
-                DiffWorkStaged -> (["--cached"],[])
-            file = [packageSpec pkg | speconly]
-        withBranch <-
-          case mwbr of
-            Nothing -> return []
-            Just wbr ->
-              let brn = show wbr in
-                if '/' `elem` brn
-                then return [brn]
-                else do
-                  localbrs <- gitLines "branch" ["--format=%(refname:short)"]
-                  if brn `elem` localbrs
-                    then return [brn]
-                    else return ["origin/" ++ brn]
-        let revdiff = case mwbr of
-              Nothing -> []
-              Just wbr -> case (wbr,br) of
-                (RelBranch rwbr, RelBranch rbr) -> ["-R" | rwbr > rbr]
-                _ -> []
-        diff <- gitLines "diff" $ contxt ++ workOpts ++ revdiff ++ withBranch ++ workArgs ++ file
-        let diffout = (maybe id filterPattern mpatt . simplifyDiff fmt) diff
-        -- FIXME: sometimes we may want to list even if diff but no diffout
-        unless (null diffout) $
-          unless (ignorebumps && isTrivialRebuildCommit diffout) $
-          if fmt == DiffQuiet
-          then putStrLn $ unPackage pkg
+      ok <-
+        case br of
+          OtherBranch _ -> gitSwitchBranch br >> return True
+          RelBranch rbr -> gitSwitchBranch' False rbr
+      when ok $ do
+        speconlyNone <-
+          if speconly
+          then notM $ doesFileExist $ packageSpec pkg
+          else return False
+        if speconlyNone
+          then do
+          dead <- doesFileExist "dead.package"
+          unless dead $ putStrLn $ "no " ++ packageSpec pkg
           else do
-            putPkgAnyBrnchHdr pkg br
-            mapM_ putStrLn diffout
+          let contxt = case fmt of
+                         DiffContext n -> ["--unified=" ++ show n]
+                         DiffMinimal -> ["--unified=0"]
+                         DiffStatus -> ["--name-status"]
+                         -- FIXME hide "files changed" and "insertions" summary
+                         DiffStats -> ["--compact-summary"]
+                         _ -> []
+              (workOpts,workArgs) =
+                case work of
+                  DiffWorkAll -> ([],["HEAD" | isNothing mwbr])
+                  DiffWorkUnstage -> ([],[])
+                  DiffWorkStaged -> (["--cached"],[])
+              file = [packageSpec pkg | speconly]
+          mwithBranch <-
+            case mwbr of
+              Nothing -> return $ Just []
+              Just wbr ->
+                let brn = show wbr in
+                  if '/' `elem` brn
+                  then return $ Just [brn]
+                  else do
+                    localbrs <- gitLines "branch" ["--format=%(refname:short)"]
+                    if brn `elem` localbrs
+                      then return $ Just [brn]
+                      else do
+                      wbexists <- checkIfRemoteBranchExists wbr
+                      if wbexists
+                        then return $ Just ["origin/" ++ brn]
+                        else do
+                          putStrLn $ "no" +-+ show wbr +-+ "for" +-+ unPackage pkg
+                          return Nothing
+          whenJust mwithBranch $ \withBranch -> do
+            let revdiff =
+                  case mwbr of
+                    Nothing -> []
+                    Just wbr -> case (wbr,br) of
+                      (RelBranch rwbr, RelBranch rbr) -> ["-R" | rwbr > rbr]
+                      _ -> []
+            diff <- gitLines "diff" $ contxt ++ workOpts ++ revdiff ++ withBranch ++ workArgs ++ file
+            let diffout = (maybe id filterPattern mpatt . simplifyDiff fmt) diff
+            -- FIXME: sometimes we may want to list even if diff but no diffout
+            unless (null diffout) $
+              unless (ignorebumps && isTrivialRebuildCommit diffout) $
+              if fmt == DiffQuiet
+              then putStrLn $ unPackage pkg
+              else do
+                putPkgAnyBrnchHdr pkg br
+                mapM_ putStrLn diffout
         where
           simplifyDiff :: DiffFormat -> [String] -> [String]
           simplifyDiff DiffMinimal ds = simplifyMinimalDiff ds
