3586ad4ff5c745ac8ec40ca586d7064983372d1c
Jens Petersen <juhpetersen@gmail.com>
2023-08-17 23:46:42 +0800

    gitFetchSilent: use \r to hide "git fetching..."

--- src/Git.hs
+++ src/Git.hs
@@ -186,7 +186,7 @@ gitFetchSilent quiet = do
                    if "From " `isPrefixOf` hd then tl else hd:tl
   if null filtered
     then unless quiet $ putStrLn "done"
-    else putStrLn $ intercalate "\n" filtered
+    else putStrLn $ '\r' : intercalate "\n" filtered
 
 checkWorkingDirClean :: IO ()
 checkWorkingDirClean = do
