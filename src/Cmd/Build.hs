juhp/fbrnch

commit 0a4243b349313ccfd1d81d7f34fa48ea9b6b3d1b

build: use target branch to determine nvr

src/Cmd/Build.hs
@@ -5,7 +5,7 @@ module Cmd.Build (
   BuildOpts(..)
   ) where
 
-import Distribution.Fedora.Branch (branchDestTag)
+import Distribution.Fedora.Branch (branchDestTag, readBranch)
 import Fedora.Krb (krbTicket)
 import SimplePrompt (promptEnter, yesNo)
 
@@ -107,9 +107,12 @@ buildBranch mlastpkg opts pkg rbr@(RelBranch br) = do
   let spec = packageSpec pkg
   checkForSpecFile spec
   checkSourcesMatch pkg (RelBranch br) spec
+  -- FIXME: check for %macros in %changelog
   unpushed <- gitOneLineLog $ "origin/" ++ showBranch br ++ "..HEAD"
-  nvr <- pkgNameVerRel' br spec
   putNewLn
+  let msidetagTarget = buildoptSidetagTarget opts
+  target <- targetMaybeSidetag dryrun True True br msidetagTarget
+  nvr <- pkgNameVerRel' (maybeTargetBranch target) spec
   mpush <-
     case unpushed of
       [] -> return Nothing
@@ -124,11 +127,17 @@ buildBranch mlastpkg opts pkg rbr@(RelBranch br) = do
         if tty && (not merged || (newrepo && ancestor && length unmerged == 1))
           then refPrompt unpushed $ "Press Enter to push and build" ++ (if length unpushed > 1 then "; or give ref to push" else "") ++ (if not newrepo then "; or 'no' to skip pushing" else "")
           else return $ Just $ commitRef unpd
-  let msidetagTarget = buildoptSidetagTarget opts
-  target <- targetMaybeSidetag dryrun True True br msidetagTarget
   buildRun spec nvr merged mpush unpushed target msidetagTarget moverride
   where
     dryrun = buildoptDryrun opts
+
+    maybeTargetBranch target =
+      if showBranch br `isPrefixOf` target
+      then br
+      else
+        case readBranch $ takeWhile (/= '-') target of
+          Just b -> b
+          Nothing -> error' $ "unknown branch for target" +-+ target
 
     buildRun spec nvr merged mpush unpushed target msidetagTarget moverride = do
       let mwaitrepo = buildoptWaitrepo opts

commit 73dd06273b57454c401bff8a71de158d73a3a3d1

build --sidetag: fix "release not found for branch f43" rawhide regression
Fixes regression introduced by 0a4243b in 1.7

src/Cmd/Build.hs
@@ -6,6 +6,7 @@ module Cmd.Build (
   ) where
 
 import Distribution.Fedora.Branch (branchDestTag, readBranch)
+import Distribution.Fedora.Release (getRawhideVersion)
 import Fedora.Krb (krbTicket)
 import SimplePrompt (promptEnter, yesNo)
 
@@ -112,7 +113,8 @@ buildBranch mlastpkg opts pkg rbr@(RelBranch br) = do
   putNewLn
   let msidetagTarget = buildoptSidetagTarget opts
   target <- targetMaybeSidetag dryrun True True br msidetagTarget
-  nvr <- pkgNameVerRel' (maybeTargetBranch target) spec
+  rawhide <- getRawhideVersion
+  nvr <- pkgNameVerRel' (maybeTargetBranch rawhide target) spec
   mpush <-
     case unpushed of
       [] -> return Nothing
@@ -131,12 +133,16 @@ buildBranch mlastpkg opts pkg rbr@(RelBranch br) = do
   where
     dryrun = buildoptDryrun opts
 
-    maybeTargetBranch target =
+    maybeTargetBranch :: Natural -> String -> Branch
+    maybeTargetBranch rawhide target =
       if showBranch br `isPrefixOf` target
       then br
       else
         case readBranch $ takeWhile (/= '-') target of
-          Just b -> b
+          Just b ->
+            if b == Fedora rawhide
+            then Rawhide
+            else b
           Nothing -> error' $ "unknown branch for target" +-+ target
 
     buildRun spec nvr merged mpush unpushed target msidetagTarget moverride = do